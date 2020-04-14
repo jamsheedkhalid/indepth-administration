b45d7c8787401b5023a6cd8f18f568c5e4a50e6b
Jamsheed Khalid <jamsheedkhalid35@gmail.com>
2020-04-14 23:20:10 +0400

    parents acces to report cards
    Signed-off-by: Jamsheed Khalid <jamsheedkhalid35@gmail.com>

--- mysql/student/fillTerms.php
+++ mysql/student/fillTerms.php
@@ -1,18 +1,20 @@
 <?php
-session_start();
-include($_SERVER['DOCUMENT_ROOT'] . '/config/database.php');
+//session_start();
+//include($_SERVER['DOCUMENT_ROOT'] . '/config/database.php');
+//
+//$user_id = $_SESSION['user'];
+////echo $user_id;
+//
+//$sql = "select exam_groups.name term from students
+//inner join users on students.user_id = users.id
+//inner join exam_groups on students.batch_id = exam_groups.batch_id
+//where users.id = '$user_id'";
+////echo $sql;
+//$result = $conn->query($sql);
+//while ($row = mysqli_fetch_array($result)) {
+//    echo $row['term']  . "\t";
+//}
+//
+//$conn->close();
 
-$user_id = $_SESSION['user'];
-//echo $user_id;
-
-$sql = "select exam_groups.name term from students
-inner join users on students.user_id = users.id
-inner join exam_groups on students.batch_id = exam_groups.batch_id
-where users.id = '$user_id'";
-//echo $sql;
-$result = $conn->query($sql);
-while ($row = mysqli_fetch_array($result)) {
-    echo $row['term']  . "\t";
-}
-
-$conn->close();
+echo 'Term 2'  . "\t";
