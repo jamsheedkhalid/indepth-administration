9ec0cf869764bc300731bb2bfc78bfca6706b2ba
Jamsheed Khalid <jamsheedkhalid35@gmail.com>
2020-11-17 10:17:26 +0400

    'fix'

--- mysql/exam-center/term-reports/generate_pdf_student_wise.php
+++ mysql/exam-center/term-reports/generate_pdf_student_wise.php
@@ -205,7 +205,7 @@ group by subjects.id; ";
             } else if ($grade === 'GR 9'
                 || $grade === 'GR10' || $grade === 'GR11' || $grade === 'GR12') {
 
-                if (strpos($row['subject'], 'Moral Education') === true ) {
+                if (strpos($row['subject'], 'Moral Education') !== false ) {
                     $ME['subject'] = $row['subject'];
                     $ME['max'] = $row['max'];
                     $ME['min'] = $row['min'];
