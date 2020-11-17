5261ea068f91aa73e622b1cf57777b118ddcb32f
Jamsheed Khalid <jamsheedkhalid35@gmail.com>
2020-11-17 13:58:01 +0400

    'fix'

--- mysql/exam-center/term-only-reports/generate_pdf_student_wise.php
+++ mysql/exam-center/term-only-reports/generate_pdf_student_wise.php
@@ -105,9 +105,8 @@ and  students.last_name = '$student'; ";
              MAX(IF(exam_groups.name = '$term - Class Evaluation', exam_scores.marks, null)) * $ass_percent / 100, 0) TR
 from students p
     inner join batches on p.batch_id = batches.id
-         left join students_subjects on p.id = students_subjects.student_id
-         inner join subjects on batches.id = subjects.batch_id and  subjects.elective_group_id is NUll 
-         left join subjects on students_subjects.subject_id = subjects.id  
+         inner join students_subjects on p.id = students_subjects.student_id
+         right join subjects on students_subjects.subject_id = subjects.id and subjects.elective_group_id is null 
          inner join courses on batches.course_id = courses.id
          inner join exams on subjects.id = exams.subject_id
          inner join exam_groups on exams.exam_group_id = exam_groups.id
