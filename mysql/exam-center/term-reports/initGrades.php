bb9126642077058ec7d4c020025539c44f17d1ec
Jamsheed Khalid <jamsheedkhalid35@gmail.com>
2021-01-05 11:54:12 +0400

    msg

--- mysql/exam-center/term-reports/initGrades.php
+++ mysql/exam-center/term-reports/initGrades.php
@@ -1,7 +1,7 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/config/database.php');
 
-$sql = 'SELECT  course_name FROM courses where is_deleted = 0 ORDER BY course_name';
+$sql = 'SELECT  course_name FROM courses where is_deleted = 0 and course_name LIKE "%GR%" ORDER BY course_name';
 //echo $sql;
 $result = $conn->query($sql);
 while ($row = mysqli_fetch_array($result)) {
