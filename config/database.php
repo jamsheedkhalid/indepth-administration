1e14f3d65d952769e1ae417767b627636d482877
Jamsheed Khalid <jamsheedkhalid35@gmail.com>
2021-04-11 08:45:25 +0400

    term 2 publish

--- config/database.php
+++ config/database.php
@@ -8,7 +8,7 @@
 $servername = 'localhost';
 $username = 'fedena';
 $password = 'fedenapw';
-$DB = 'alsanawabar1';
+$DB = 'alsanawabar';
 
 $conn = new mysqli($servername, $username, $password, $DB);
 
